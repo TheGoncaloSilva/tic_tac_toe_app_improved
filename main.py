b810c16f312cb41a9a3e180d7a98f613426f6a5d
Gonçalo <goncalolsilva@ua.pt>
2022-01-03 00:45:41 +0000

    Fixed errors with images nor appearing, main menu almost completed and solo mode is in progress.

--- main.py
+++ main.py
@@ -10,17 +10,17 @@ from kivy.uix.button import Button # import buttons
 from kivy.uix.label import Label # Import the simbols and widgets
 from kivy.uix.popup import Popup # Import Popups
 from kivy.uix.boxlayout import BoxLayout # Box layout for Popup
-from src.pages.common import Options_modals
+from src.pages.common import Options_modals, analyze_moves
 from kivy.properties import ObjectProperty
 from kivy.properties import StringProperty
 from kivy.lang.parser import global_idmap
+from kivy import utils
 
 class Home(Screen):
     pass
 
 class Soloplayer(Screen):
     pass
-
 
 class Multiplayer(Screen):
     pass
@@ -31,25 +31,26 @@ class Lan(Screen):
 class MyApp(App):
     mode = ""
     user_choice = ""
-    all_btns_ids = []
+    all_btns_ids = [] # holds every button as an asset
     btns_ids = []
     turn = True
-    #player1 = TicTacToe()
-    #player2 = TicTacToe()
+    #player1 = Player()
+    #player2 = Player()
     winner = ''
     found_winner = False
 
     def build(self): # Construir o UI
         self.title = 'Tic Tac Toe' # Change the the name of the application window
         size_x = 960
-        size_y = 540
+        #size_y = 540
+        size_y = 700
         Window.size = (size_x, size_y)
         Window.minimum_width, Window.minimum_height = (size_x, size_y)
         return Builder.load_file('main.kv')
     
     def on_start(self):
         """
-        Function executud on app load
+        Function executed on app load
         """
         pass
 
@@ -75,12 +76,66 @@ class MyApp(App):
             box = BoxLayout(orientation='horizontal', spacing=5)
             for j in range(3):
                 btn = Button(text=f'{i}-{j}', background_normal='', color=[0, 0, 0, 0],
-                             background_down='', background_color=[1, 1, 1, 1])
+                             background_down='', background_color=utils.get_color_from_hex("#fdcb01"))
                 if not(btn in self.all_btns_ids):
                     self.all_btns_ids.append(btn)
-                btn.bind(on_release=self.change_icon)
+                btn.bind(on_release=self.make_play)
                 box.add_widget(btn)
             net.box.add_widget(box)
+
+    def make_play(self, asset):
+        self.btns_ids.append(asset)
+        if self.user_choice == 'x':
+            unchecked = 'src/img/x_button.png'
+            self.user_choice = 'o'
+        elif self.user_choice == 'o':
+            unchecked = 'src/img/o_button.png'
+            self.user_choice = 'x'
+        self.turn = not self.turn
+        
+        if asset.background_normal == '':
+            asset.background_color = [1, 1, 1, 1]
+            asset.background_normal = unchecked
+            self.get_id(asset.text)
+        else: pass
+        """
+        if (self.turn == False and self.mode == 'solo') and self.found_winner != True:
+            self.computer_player()"""
+
+    def get_id(self, member, get_num=False):
+        ids = {'0-0': 0, '0-1': 1, '0-2': 2, '1-0': 3,
+               '1-1': 4, '1-2': 5, '2-0': 6, '2-1': 7, '2-2': 8}
+        if get_num == True:
+            keys = list(ids.keys()) # prints 0-0, 0-1...
+            values = list(ids.values()) # prints the integers 1, 2 ...
+            index = values.index(member) # prints the index of a keys
+            return keys[index]
+        else:
+            res = analyze_moves(self.table_array())
+            if res != None:
+                self.game_ended(res)
+            #self.check_win(ids[member])
+
+    # Returns an array containing the positions and values of the table
+    def table_array(self):
+        table = []
+
+        for idd in self.all_btns_ids:
+            if idd.background_normal == 'src/img/x_button.png':
+                table.append('x')
+            elif idd.background_normal == 'src/img/o_button.png':
+                table.append('o')
+            else:
+                table.append(' ')
+        
+        print(table) # Debugging
+        return table
+
+    # Closes the game 
+    def game_ended(self, data):
+        #score = Score(winner, app, mode)
+        #Clock.schedule_once(lambda x: score.open(), 1)
+        pass
 
 if __name__ == "__main__":
     app = MyApp()
--- src/pages/common.py
+++ src/pages/common.py
@@ -16,6 +16,7 @@ class Options_modals(Popup):
         self.height = 250
         self.width = 310
         self.auto_dismiss = False
+        self.background_color =  utils.get_color_from_hex("#250448")
         # title
         self.title = "choose your symbol".upper()
         self.title_size = '16sp'
@@ -26,11 +27,11 @@ class Options_modals(Popup):
         # content
         self.box = BoxLayout(size_hint_y=.7, padding=10, spacing=10, pos_hint={
                              'top': .99, 'center_x': .5}, orientation='horizontal')
-        self.x_btn = Button(text="X", background_normal='', background_down='', background_color=[
-                            1, 1, 1, 1], color=[.2, .4, .4, 1], font_size="70sp", bold=True)
-        self.o_btn = Button(text="O", background_normal='', background_down='', background_color=[
-                            1, 1, 1, 1], color=[.2, .4, .4, 1], font_size="70sp", bold=True)
-        self.save_btn = Button(text="SAVE", background_normal='', background_color=[1, 1, 1, 1], color=[
+        self.x_btn = Button(text="X", background_normal='', background_down='', background_color= 
+                            utils.get_color_from_hex("#00ebea"), color=[0, 0, 0, 1], font_size="70sp", bold=True)
+        self.o_btn = Button(text="O", background_normal='', background_down='', background_color=
+                            utils.get_color_from_hex("#f60261"), color=[1, 1, 1, 1], font_size="70sp", bold=True)
+        self.save_btn = Button(text="SAVE", background_normal='', background_color=[255, 215, 0, 1], color=[
                                0, 0, 0, 1], font_size="18sp", bold=True, pos_hint={'center_x': .5, "y": .03}, size_hint=[.9, .15])
         # callbacks
         self.x_btn.bind(on_release=lambda x: self.colorize('x'))
@@ -47,13 +48,45 @@ class Options_modals(Popup):
     def colorize(self, symbol):
         if symbol == "x":
             self.x_btn.background_color = utils.get_color_from_hex("#847878")
-            self.o_btn.background_color = [1, 1, 1, 1]
+            self.o_btn.background_color = utils.get_color_from_hex("#f60261")
+            self.o_btn.color=[1, 1, 1, 1]
         elif symbol == "o":
             self.o_btn.background_color = utils.get_color_from_hex("#847878")
-            self.x_btn.background_color = [1, 1, 1, 1]
+            self.x_btn.background_color = utils.get_color_from_hex("#00ebea")
+            self.x_btn.color = [0, 0, 0, 1]
         self.app.user_choice = symbol
 
     def start_game(self):
         if self.app.user_choice != "":
             self.app.draw_net(self.mode)
             self.dismiss()
+
+# Evaluate each move made
+def analyze_moves(board):
+    result = analyze_winner(board)
+    if result[0]:
+        return ['winner', result[1]]
+
+    for x in board:
+         if x == ' ': return # if empty spaces, game continues
+
+    return ['tie'] # no empty spaces or winners
+
+# Function for dicovering the winner
+def analyze_winner(board):
+    # all strategies possible
+    strategies = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
+                  [0, 3, 6], [1, 4, 7], [2, 5, 8],
+                  [0, 4, 8], [2, 4, 6]]
+
+    for player in ['x','o']:    
+        for case in strategies:
+            match = 0
+            for pos in case:
+                if board[pos] == player:
+                    match += 1
+            
+            if match == 3:
+                return [True, player]
+
+    return [False, 1]
